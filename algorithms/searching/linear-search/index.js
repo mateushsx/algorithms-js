dcbf7deeb18644d8f0ced0a55f3df56dba1232f3
Mateus Henrique <mateusdzn10@gmail.com>
2024-09-15 20:00:16 -0400

    feat: :sparkles: adds runtime logs and unorders the list

--- algorithms/searching/linear-search/index.js
+++ algorithms/searching/linear-search/index.js
@@ -1,11 +1,17 @@
-const elements = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const elements = [2, 8, 3, 1, 5, 6, 7, 9, 10, 11, 15, 13, 12, 14];
 const elementToFind = 11;
+
+const startTime = Date.now();
+let executedTime = 0;
 
 function linearSeach(array, item) {
   if (!Array.isArray(array)) return -1;
 
   for (let i = 0; i < array.length; i++) {
-    if (array[i] === item) return item;
+    if (array[i] === item) {
+      executedTime = Date.now() - startTime;
+      return item;
+    }
   }
 
   return -1;
@@ -14,3 +20,4 @@ function linearSeach(array, item) {
 const elementFoud = linearSeach(elements, elementToFind);
 
 console.log('Elemento:', elementFoud);
+console.log(`Tempo de execução: ${executedTime}ms`);
