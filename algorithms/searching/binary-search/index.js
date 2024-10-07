066084cbd3e85e1d0cf2768ad48080da271762aa
K-Zoldyck <devv-robert@outlook.com>
2024-10-07 14:41:37 -0300

    fix: refactored code to to test tools
    ....

--- algorithms/searching/binary-search/index.js
+++ algorithms/searching/binary-search/index.js
@@ -1,7 +1,9 @@
-const elements = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
-const elementToFind = 11;
 
-const startTime = Date.now();
+/* 
+  const elements = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+  const elementToFind = 11;
+  const startTime = Date.now(); 
+*/
 
 function binarySearch(array, item) {
   if (!Array.isArray(array)) return -1;
@@ -25,7 +27,10 @@ function binarySearch(array, item) {
   return -1;
 }
 
-const elementFoud = binarySearch(elements, elementToFind);
+module.exports = binarySearch;
 
-console.log('Indice do elemento:', elementFoud);
-console.log(`Tempo de execução: ${Date.now() - startTime}ms`);
+/*
+  const elementFoud = binarySearch(elements, elementToFind);
+  console.log('Indice do elemento:', elementFoud);
+  console.log(`Tempo de execução: ${Date.now() - startTime}ms`);
+*/
